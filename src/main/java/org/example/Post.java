1c57f989b0d40f80d276d405d13f36afd2338723
Svetlana Tokarchuk <lanawm73@gmail.com>
2022-10-23 04:38:08 -0400

    Files added

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+    private final int id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvotes;
+
+    public Post(
+            @JsonProperty("id") int id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
